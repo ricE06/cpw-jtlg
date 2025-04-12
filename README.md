773646a2d6c6fc37ec2aadc2171c378510cfe792
ricE06 <ezzhaneri06@gmail.com>
2025-04-11 21:02:55 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cpw-jtlg
 A website to host the 2025 CPW Jet Lag The Game event.
+
+## Installation
+
+First, clone this repository locally. This app requires Flask,
+which can be pip-installed:
+
+```
+pip install flask
+```
---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)  # I think this is right? Should be for single module
+
+@app.route("/")
+def index():
+    # todo: make the homepage and put it into a html file
+    return "<p>Hello, World!</p>" 
+
